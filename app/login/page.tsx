374b005a2925bd880c2676eb5d03c76386b94a09
Juan Torres <jdtorreserazo@gmail.com>
2024-11-01 08:34:31 -0700

    Updated login icon

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,13 @@ export default function LoginPage() {
     <section id="login" className="block lg:flex h-svh">
       {/* Left or Top section */}
       <div className="h-[60%] lg:h-full w-full lg:w-[70%]">
-        <video src="/videos/loopLogo.mp4" className="object-cover h-full w-auto" autoPlay muted loop />
+        <video
+          src="/videos/loopLogo.mp4"
+          className="object-cover h-full w-auto"
+          autoPlay
+          muted
+          loop
+        />
       </div>
 
       {/* Right or Bottom section */}
--- components/login/LoginCard.tsx
+++ components/login/LoginCard.tsx
@@ -1,14 +1,23 @@
+"use client";
 import Image from "next/image";
+import {useRouter} from "next/navigation";
 
 export default function LoginCard() {
+  const router = useRouter();
+  const handleLogin = () => {
+    router.push("/dashboard");
+  };
+
   return (
     <div id="loginCard">
       <div className="w-full  text-center py-3 border-b-black/50 border-b-2">
         <h1 className="text-black/60 font-medium uppercase">Ingrese a su cuenta</h1>
       </div>
-      <div className="w-full flex-grow flex flex-col items-center justify-center pt-4 pb-8">
-        <Image src="/images/userIcon.png" alt="userIcon" width={110} height={110} />
-        <button className="btn btn-primary">Iniciar Sesión</button>
+      <div className="w-full flex-grow flex flex-col items-center justify-center gap-4 pt-4 pb-8">
+        <Image src="/images/logo.png" alt="userIcon" width={80} height={80} />
+        <button className="btn btn-primary" onClick={handleLogin}>
+          Iniciar Sesión
+        </button>
       </div>
     </div>
   );
