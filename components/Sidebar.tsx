87d9111da9ff84e00ef8436d5401f1b90aa88772
Juan Torres <jdtorreserazo@gmail.com>
2024-11-02 12:04:07 -0700

    fixed sidebar useState import

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,6 @@
 "use client";
 import {faX} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {useState} from "react";
-
 type SidebarProps = {
   menuOpen: boolean;
   toogleMenu: () => void;
