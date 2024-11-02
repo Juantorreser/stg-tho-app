3937a358b605fd0bb4469c12bce01c057128f24a
Juan Torres <jdtorreserazo@gmail.com>
2024-11-02 11:58:26 -0700

    sidebar padding adjusted

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar: React.FC<SidebarProps> = ({menuOpen, toogleMenu}) => {
   return (
     <aside
       id="sidebar"
-      className={`h-svh p-0 lg:p-10 absolute left-0 lg:relative bg-foreground 
+      className={`h-svh p-0 lg:p-8 absolute left-0 lg:relative bg-foreground 
       ${menuOpen ? "w-[70%] lg:w-[15%]" : "collapsed"}`}>
       <div className="text-white">
         <FontAwesomeIcon icon={faX} onClick={toogleMenu} />
