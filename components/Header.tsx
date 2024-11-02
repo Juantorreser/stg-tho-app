5f4f1fc54f7b41fc5e57d28c714b34465d708e9d
Juan Torres <jdtorreserazo@gmail.com>
2024-11-02 12:38:22 -0700

    Header userInfor updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBell, faCog, faEnvelope, faUser, faBars, faX} from "@fortawesome/free-solid-svg-icons";
+import {faBell, faCog, faEnvelope, faBars, faChevronLeft} from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import {faChevronDown} from "@fortawesome/free-solid-svg-icons/faChevronDown";
 
 type HeaderProps = {
   menuOpen: boolean;
@@ -9,21 +11,32 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({toogleMenu, menuOpen}) => {
   return (
     <nav id="header">
-      <div className="cursor-pointer" onClick={toogleMenu}>
-        <FontAwesomeIcon icon={menuOpen ? faX : faBars} width={25} />
+      <div
+        className="cursor-pointer rounded-full bg-opacity-35 bg-white/20 w-[40px] h-[40px] flex items-center justify-center"
+        onClick={toogleMenu}>
+        <FontAwesomeIcon icon={menuOpen ? faChevronLeft : faBars} width={25} />
       </div>
       <ul>
         <li className="icon">
-          <FontAwesomeIcon icon={faBell} width={15} />
+          <FontAwesomeIcon icon={faBell} width={25} />
         </li>
         <li className="icon">
-          <FontAwesomeIcon icon={faEnvelope} width={15} />
+          <FontAwesomeIcon icon={faEnvelope} width={25} />
         </li>
         <li className="userInfo">
-          <FontAwesomeIcon icon={faUser} width={15} />
+          <Image src={"/images/userIcon.png"} width={40} height={40} alt="Profile Image" />
+          <div className="hidden md:block">
+            <p className="text-[0.65rem] p-0 m-0">Admin</p>
+            <p className="text-xs font-medium p-0 m-0 flex items-center gap-1">
+              Jhon Doe
+              <span className="text-[0.55rem] font-bold">
+                <FontAwesomeIcon icon={faChevronDown} />
+              </span>
+            </p>
+          </div>
         </li>
         <li className="icon">
-          <FontAwesomeIcon icon={faCog} width={15} />
+          <FontAwesomeIcon icon={faCog} width={25} />
         </li>
       </ul>
     </nav>
